5b1248a2473697eab6ef902a88979382ce2d35d6
Art Todorov <art.todorov@me.com>
2020-01-02 11:11:08 -0800

    delete & filter tasks

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -41,9 +47,41 @@ function addTask(e) {
 
   // Clear input
   taskInput.value = '';
-
-  console.log(li);
+  // console.log(li);
   
-
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks(){
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+  
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
